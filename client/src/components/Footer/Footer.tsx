73768ba3b0b09022dc35231d5df7f297c7d131df
DinaDi025 <di.serdyuck0201@gmail.com>
2024-07-08 21:37:28 +0300

    add footer components

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+import styles from "./Footer.module.scss";
+
+const Footer: FC = () => {
+  return <div className={styles.container}></div>;
+};
+
+export { Footer };
