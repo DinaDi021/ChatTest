1f17825961516797c5a126fd0723e9e048d4513f
DinaDi025 <di.serdyuck0201@gmail.com>
2024-07-08 21:36:46 +0300

    creat page

---
+++ client/src/pages/AuthPage/LoginPage.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import styles from "../pages.module.scss";
+
+const LoginPage: FC = () => {
+  return <div className={styles.auth__page}>Login</div>;
+};
+
+export { LoginPage };
