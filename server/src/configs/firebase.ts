772cb9aba322b882e7d4ca6d82e7c29d62cd5bdb
DinaDi025 <di.serdyuck0201@gmail.com>
2024-07-08 23:20:33 +0300

    firebase path

--- server/src/configs/firebase.ts
+++ server/src/configs/firebase.ts
@@ -1,7 +1,7 @@
 import * as admin from "firebase-admin";
 import { ServiceAccount } from "firebase-admin";
 
-import serviceAccount from "../../serviceAccount.json"
+import serviceAccount from "../serviceAccount.json";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as ServiceAccount),
