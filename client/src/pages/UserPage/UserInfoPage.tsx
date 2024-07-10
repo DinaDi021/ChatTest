64575d6fa90cbf07ead695d290fcfd76882ea5d4
DinaDi025 <di.serdyuck0201@gmail.com>
2024-07-10 10:25:27 +0300

    add user page

--- client/src/pages/UserPage/UserInfoPage.tsx
+++ client/src/pages/UserPage/UserInfoPage.tsx
@@ -1,7 +1,28 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Account } from "../../components";
+import { useAppSelector } from "../../hooks";
+import styles from "../pages.module.scss";
 
 const UserInfoPage: FC = () => {
-  return <div>Info</div>;
+  const { me } = useAppSelector((state) => state.auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!me) {
+      navigate("/login");
+    }
+  }, [me, navigate]);
+
+  if (!me) {
+    return <p>user not logged</p>;
+  }
+  return (
+    <div className={styles.user__page}>
+      <Account />
+    </div>
+  );
 };
 
 export { UserInfoPage };
