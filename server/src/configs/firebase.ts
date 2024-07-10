52da7c00be0706ca041b0c58ae7b6d90b712d182
DinaDi025 <di.serdyuck0201@gmail.com>
2024-07-10 10:27:20 +0300

    db.settings

--- server/src/configs/firebase.ts
+++ server/src/configs/firebase.ts
@@ -1,13 +1,13 @@
 import * as admin from "firebase-admin";
-import { ServiceAccount } from "firebase-admin";
 
 import serviceAccount from "../serviceAccount.json";
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as ServiceAccount),
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
 
 const db = admin.firestore();
+// db.settings({ ignoreUndefinedProperties: true });
 const auth = admin.auth();
 
 export { admin, db, auth };
