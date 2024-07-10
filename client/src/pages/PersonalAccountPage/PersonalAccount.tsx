35f457bf6fa0948bffca87f5307970b1c92cb7f4
DinaDi025 <di.serdyuck0201@gmail.com>
2024-07-10 10:26:15 +0300

    del

--- client/src/pages/PersonalAccountPage/PersonalAccount.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-
-import styles from "../pages.module.scss";
-
-const PersonalAccount: FC = () => {
-  return <div className={styles.personalAccount}>Your page</div>;
-};
-
-export { PersonalAccount };
