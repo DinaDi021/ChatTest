1ef23f6b847fcff11a3a342e11f7b476f99dc7c3
DinaDi025 <di.serdyuck0201@gmail.com>
2024-07-10 14:05:46 +0300

    token type

--- server/src/types/token.types.ts
+++ server/src/types/token.types.ts
@@ -1,5 +1,3 @@
-import { EActionTokenType } from "../enums/actionTokenType";
-
 export interface ITokenPayload {
   userId: string;
 }
@@ -8,18 +6,3 @@ export interface ITokensPair {
   accessToken: string;
   refreshToken: string;
 }
-
-export interface IToken extends Document {
-  accessToken: string;
-  refreshToken: string;
-  userId: string;
-}
-
-export interface IActionTokenDocument extends Document {
-  token: string;
-  type: EActionTokenType;
-  userId: string;
-}
-
-export interface IActionToken
-  extends Pick<IActionTokenDocument, "token" | "type" | "userId"> {}
