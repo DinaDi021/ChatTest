b34978d59ba807a4c8c0dcbad40a8b608dad6fbc
DinaDi025 <di.serdyuck0201@gmail.com>
2024-07-14 07:21:54 +0300

    add user avatar

--- server/src/configs/configs.ts
+++ server/src/configs/configs.ts
@@ -15,4 +15,14 @@ export const configs = {
 
   NO_REPLY_EMAIL: process.env.NO_REPLY_EMAIL,
   NO_REPLY_PASSWORD: process.env.NO_REPLY_PASSWORD,
+
+  firebaseConfig: {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  },
 };
